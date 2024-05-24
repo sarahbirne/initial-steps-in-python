713577daa6d4917159f1a13001d85c88a324b390
Sarah Birne <154691526+sarahbirne@users.noreply.github.com>
2024-05-24 17:51:21 -0300

    IMC calculator

---
+++ imc.py
@@ -0,0 +1,21 @@
+# calculadora de IMC
+nome = input('Digite seu nome: ')
+altura = float(input('Digite sua altura (em metros): '))
+peso = float(input('Digite seu peso (em kg): '))
+imc = peso / (altura ** 2)
+
+print(f'Olá, {nome}. Vamos calcular seu IMC?')
+print(f'Seu IMC é: {imc:.2f}')
+
+if imc < 18.4:
+    print('Categoria: Subpeso')
+elif 18.5 <= imc < 24.9:
+    print('Categoria: Normal')
+elif 25 <= imc < 29.9:
+    print('Categoria: Sobrepeso')
+elif 30 <= imc < 34.9:
+    print('Categoria: Obesidade grau 1')
+elif 35 <= imc < 39.9:
+    print('Categoria: Obesidade grau 2')
+else:
+    print('Categoria: Obesidade grau 3')
